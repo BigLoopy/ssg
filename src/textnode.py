b4d0bafd2c08b16f44fd1eff454c67e8b4a3b5a8
BigLoopy <plupiani@gmail.com>
2025-01-07 11:31:13 -0500

    Chapter 2 - unit tests

--- src/textnode.py
+++ src/textnode.py
@@ -19,7 +19,8 @@ class TextNode():
         #print(f"--->self.url = {url}")
     
     def __eq__(self, value):
-        return self.text == value.text and self.text_type == value.text_type and self.url == value.UnicodeTranslateError
+        return self.text == value.text and self.text_type == value.text_type and self.url == value.url
+    
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
